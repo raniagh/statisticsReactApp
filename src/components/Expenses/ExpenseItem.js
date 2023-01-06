3fc6f1951c8f7f01924f84d77b0fb6b12602daa0
Your Name <rghzaiel@ggn-group.com>
2023-01-06 12:23:20 +0100

    statistics app part1

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,17 @@
+import Card from "../../UI/Card";
+import ExepenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExepenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
